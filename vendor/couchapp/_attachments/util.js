2edf95144fad6a01e5c0f4b92cbca514787aec08
Ziv Kennan <zkennan@hyacinth.(none)>
2012-07-23 17:11:34 +0800

    final count code

--- vendor/couchapp/_attachments/util.js
+++ vendor/couchapp/_attachments/util.js
@@ -10,3 +10,14 @@ function registerUser(version) {
       "json");
 }
 
+function registerSugg(version) {
+  var restFrag = '_update/incrVisitCount/visitCounts';
+  var doc = new Object();
+  doc.v1c = 0; doc.v2c = 0; doc.v1sc = 0; doc.v2sc = 0;
+  if (version == 1) { doc.v1sc = 1; } else { doc.v2sc = 1; }
+  var postData = JSON.stringify(doc);
+  $.post(restFrag, postData,
+      function(data) {
+      },
+      "json");
+}
